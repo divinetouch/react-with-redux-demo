d3e2c828967993f876405daab497ef47f72009cd
divine touch <divine.touch07@gmail.com>
2016-01-03 16:43:25 -0600

    add more unit tests demo

--- tests/views/checkout.spec.js
+++ tests/views/checkout.spec.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Checkout from '../../app/views/checkout';
+import CheckoutItem from '../../app/components/checkout/checkout-item';
 import TestUtils from 'react-addons-test-utils';
 import expect from 'expect';
 import expectJSX from 'expect-jsx';
 import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import reducers from '../../app/reducers/reducers';
 expect.extend(expectJSX);
 
@@ -15,8 +17,6 @@ describe('checkout page component', () => {
         cost: 5,
         description: 'this is item 1'
     };
-
-
 
     it('should render checkout page', () => { 
 
@@ -32,5 +32,33 @@ describe('checkout page component', () => {
         const expected = 'this is item 1';
 
         expect(actual).toIncludeJSX(expected);
+    });
+
+    it('should render checkout page - test with renderToStaticMarkup', () => { 
+
+        store.dispatch({ 
+            type: 'ADD_TO_CART',
+            item: item,
+            quantity: 5
+        });
+
+        let elementMarkUp = React.renderToStaticMarkup(<Provider store={store}><Checkout /></Provider>);
+        const actual = elementMarkUp;
+        const expected = item.name;
+
+        expect(actual).toIncludeJSX(expected);
+    });
+
+    it('should render CheckItem component', () => { 
+        store.dispatch({ 
+            type: 'ADD_TO_CART',
+            item,
+            quantity: 5
+        });
+
+        let element = TestUtils.renderIntoDocument(<Provider store={ store }><Checkout /></Provider>);
+        let actual = TestUtils.findRenderedComponentWithType(element, CheckoutItem);
+        
+        expect(actual).toExist();
     });
 });
